c23618b7b12b372ab0a9cb4c8aebbb2b4ce0de5e
Hayleigh Thompson <me@hayleigh.dev>
2024-04-01 09:31:20 +0100

    :recycle: Emit built css to priv/static.

--- test/build.ffi.mjs
+++ test/build.ffi.mjs
@@ -10,7 +10,7 @@ import nesting from "postcss-nesting";
 const compiler = postcss([nesting, autoprefixer, cssnano]);
 
 export const compile_css = async (css, name, next) => {
-  const to = join(cwd(), `./priv/${name}.css`);
+  const to = join(cwd(), `./priv/static/${name}.css`);
   const { css: out } = await compiler.process(css, { from: undefined, to });
   await writeFile(to, out, { encoding: "utf8" });
 
