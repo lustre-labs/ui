b8089ab999ed96517b2aa5eab1180cfd85cd2f65
Hayleigh Thompson <me@hayleigh.dev>
2024-10-03 22:54:07 +0100

    :sparkles: Create a lustre-ui-intersection-observer custom element for declarative intersection observer use.

--- src/dom.ffi.mjs
+++ src/dom.ffi.mjs
@@ -32,3 +32,55 @@ export const assigned_elements = (slot) => {
 
   return new Error(List.fromArray([]));
 };
+
+/**
+ *
+ */
+export const animation_time = () => {
+  return window.document.timeline.currentTime;
+};
+
+// COMPONENTS ------------------------------------------------------------------
+
+customElements.define(
+  "lustre-ui-intersection-observer",
+  class LustreUiIntersectionObserver extends HTMLElement {
+    constructor() {
+      super();
+      this.observer = null;
+    }
+
+    connectedCallback() {
+      const options = {
+        root: null,
+        rootMargin: "0px",
+        threshold: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1],
+      };
+
+      this.observer = new IntersectionObserver((entries) => {
+        for (const entry of entries) {
+          const intersectionEvent = new CustomEvent("intersection", {
+            detail: {
+              target: entry.target,
+              isIntersecting: entry.isIntersecting,
+              intersectionRatio: entry.intersectionRatio,
+            },
+            bubbles: true,
+            composed: true,
+          });
+
+          this.dispatchEvent(intersectionEvent);
+        }
+      }, options);
+
+      // Observe self
+      this.observer.observe(this);
+    }
+
+    disconnectedCallback() {
+      if (this.observer) {
+        this.observer.disconnect();
+      }
+    }
+  },
+);
