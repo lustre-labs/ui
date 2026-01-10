4fa93475f4be5eca15e07949feccb9a74d565e44
Hayleigh Thompson <me@hayleigh.dev>
2026-01-11 00:19:25 +0100

    :fire: Nuke old stuff.

--- src/lustre/ui/otp.ffi.mjs
+++
@@ -1,5 +0,0 @@
-export const reset_selection = (root) => {
-  const input = root.querySelector("input.otp-input");
-
-  input.selectionStart = input.selectionEnd = input.value.length;
-};
--- src/lustre/ui_internal/focus.ffi.mjs
+++
@@ -1,3 +0,0 @@
-export const apply = (element) => {
-  element.focus();
-};
